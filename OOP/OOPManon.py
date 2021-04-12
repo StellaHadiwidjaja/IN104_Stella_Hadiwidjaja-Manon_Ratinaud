38bba989866da3ef5de171f535b15345f790e09b
manonratinaud <43268685+manonratinaud@users.noreply.github.com>
2021-04-12 16:50:13 +0200

    TD2 IN104

--- OOP/OOPManon.py
+++ OOP/OOPManon.py
@@ -5,7 +5,7 @@ class Books:
         self.author=author
         
     def getTitle(self):
-        return self.name
+        return self.title
     
     def getAuthor(self):
         return self.author
@@ -16,36 +16,34 @@ class Books:
 class Novels(Books):
     
     def __init__(self,title,author,protagonist,language):
-        super().__init__(self,title,author)
+        super().__init__(title,author)
         self.protagonist=protagonist
         self.language=language
     
     def summary(self):
-        print("The main character in"+ self.getTitle() + "is" + self.protagonist+".")
+        print("The main character in "+ self.getTitle() + " is " + self.protagonist+".")
         
     def thrilling(self):
         print("This book is thrilling !")
     
-    def language(self):
-        print("This book is in"+self.language)
+    def getLanguage(self):
+        print("This book is in "+ self.language)
         
     
 class Theater(Books):
     
     def __init__(self,title,author,acts,year):
-        super().__init__(self,title,author)
+        super().__init__(title,author)
         self.acts=acts
         self.year=year
     
-    def representation(director,theatre):
-        print("This theatre piece written in" + self.year+ "by" +self.getAuthor()+"is performed at the" + theatre + "and is directed by" + director)
+    def representation(self,director,theatre):
+        print("This theatre piece written in " + str(self.year)+ " by " +self.getAuthor()+" is performed at the " + theatre + " and is directed by " + director)
         
-    def adaptation(year,author):
-        print("In"+year+","+author+"adapted the theater piece of "+self.getAuthor())
+    def adaptation(self,year,author):
+        print("In "+str(year)+", "+author+" adapted the theater piece of "+self.getAuthor())
         self.author=author
         self.year=year
     
 book1= Novels("The Testaments","Margaret Atwood","Aunt Lydia","English")
 book2= Theater("Fin de Partie", "Samuel Beckett",1,1957)
-    
-    
